41619f2dd897455cfcf0210e3c41073203699383
chadyeh <chadyeh.yc@outlook.com>
2019-04-27 14:44:43 +0800

    Modify 完成 lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
 import { AuthModule } from './auth/auth.module';
-import { PostsModule } from './posts/posts.module';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, NotFoundComponent],
-  imports: [BrowserModule, LayoutModule, AppRoutingModule, AuthModule, PostsModule, HttpClientModule],
+  imports: [BrowserModule, LayoutModule, AppRoutingModule, AuthModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
