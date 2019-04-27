a8dc7c048c6c5dbc334390a7f9458d87380074fe
chadyeh <chadyeh.yc@outlook.com>
2019-04-27 09:47:11 +0800

    AppModule 的 imports 加入 AuthModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, LayoutModule, AppRoutingModule],
+  imports: [BrowserModule, LayoutModule, AppRoutingModule, AuthModule],
   providers: [],
   bootstrap: [AppComponent]
 })
