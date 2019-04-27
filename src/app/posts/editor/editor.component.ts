1136e98fb8ec070128d87c562f4c8d0d284dff50
chadyeh <chadyeh.yc@outlook.com>
2019-04-27 16:16:44 +0800

    Modify Model-driven Form 繫結表單資料

--- src/app/posts/editor/editor.component.ts
+++ src/app/posts/editor/editor.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-editor',
@@ -7,18 +7,37 @@ import { FormGroup, FormControl, FormArray } from '@angular/forms';
   styleUrls: ['./editor.component.css']
 })
 export class EditorComponent implements OnInit {
-post = new FormGroup({
-  title: new FormControl('default title'),
-  body: new FormControl(),
-  tags: new FormArray([
-    new FormControl('angular'),
-    new FormControl('HTML'),
-    new FormControl('CSS')
-  ])
-});
-  constructor() { }
+  form = new FormGroup({
+    title: new FormControl('hello'),
+    body: new FormControl('test'),
+    tags: new FormArray([
+      new FormControl('Angular'),
+      new FormControl('React'),
+      new FormControl('Vue')
+    ])
+  });
 
-  ngOnInit() {
+  get tags(): FormArray {
+    return this.form.get('tags') as FormArray;
   }
 
+  constructor() {}
+
+  ngOnInit() {}
+
+  submit() {
+    console.log(this.form.value);
+  }
+
+  addTags(tagInput: HTMLInputElement) {
+    tagInput.value.split(',')
+      .map(tagName => tagName.trim())
+      .filter(tagName => !!tagName)
+      .filter(tagName => (this.tags.value as string[]).indexOf(tagName) === -1)
+      .forEach(tagName => {
+        this.tags.push(new FormControl(tagName));
+      });
+    tagInput.value = '';
+    tagInput.focus();
+  }
 }
