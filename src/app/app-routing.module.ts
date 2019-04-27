deaa655178ab0a99c24a93eccc5e5e9a65ab378e
chadyeh <chadyeh.yc@outlook.com>
2019-04-27 10:20:50 +0800

    Add not found page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,52 +1,50 @@
-import {
-  NgModule, Component
-} from '@angular/core';
-import {
-  RouterModule,
-  Routes
-} from '@angular/router';
-import {
-  MainComponent
-} from './layout/main/main.component';
-import {
-  LoginComponent
-} from './auth/login/login.component';
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './layout/main/main.component';
+import { LoginComponent } from './auth/login/login.component';
 import { PostsComponent } from './posts/posts/posts.component';
 import { PostComponent } from './posts/post/post.component';
 import { EditorComponent } from './posts/editor/editor.component';
+import { NOTFOUND } from 'dns';
+import { NotFountComponent } from './not-fount/not-fount.component';
 
-const routes: Routes = [{
-  path: '',
-  component: MainComponent,
-  children: [
-    {
-      path: '',
-      redirectTo: 'posts',
-      pathMatch: 'full'
-    },
-    {
-      path: 'posts',
-      component: PostsComponent
-    },
-    {
-      path: 'post',
-      redirectTo: 'posts',
-      pathMatch: 'full'
-    },
-    {
-      path: 'post/:id',
-      component: PostComponent
-    },
-    {
-      path: 'create',
-      component: EditorComponent
-    }
-  ]
-},
-{
-  path: 'login',
-  component: LoginComponent
-}
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'posts',
+        pathMatch: 'full'
+      },
+      {
+        path: 'posts',
+        component: PostsComponent
+      },
+      {
+        path: 'post',
+        redirectTo: 'posts',
+        pathMatch: 'full'
+      },
+      {
+        path: 'post/:id',
+        component: PostComponent
+      },
+      {
+        path: 'create',
+        component: EditorComponent
+      }
+    ]
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+  {
+    path: '**',
+    component: NotFountComponent
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
 import { AuthModule } from './auth/auth.module';
 import { PostsModule } from './posts/posts.module';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NotFoundComponent],
   imports: [BrowserModule, LayoutModule, AppRoutingModule, AuthModule, PostsModule],
   providers: [],
   bootstrap: [AppComponent]
